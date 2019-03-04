89b9d83fcf603f46d54f93280b56d0e2a09ad617
Carl-Johann SIMON-GABRIEL <cjsimon@tuebingen.mpg.de>
2019-03-04 17:53:41 +0100

    [M] Create an abstract class Sampler; rewrote extract_img_patch without pytorch; samplers now return both W_ and X_

--- foolbox/attacks/improved_boundary_samplers.py
+++ foolbox/attacks/improved_boundary_samplers.py
@@ -1,39 +1,74 @@
+import abc
 import random
+import sys
 import numpy as np
 from .improved_boundary_utils import get_label
+
+abstractmethod = abc.abstractmethod
+
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC
+else:  # pragma: no cover
+    ABC = abc.ABCMeta('ABC', (), {})
 
 # TODO: sample images without pytorch
 # TODO: rewrite extract_img_patch function without pytorch
 # TODO: check image format
 
 
-class PixelSampler(object):
-    def __init__(self, perturb_size=.5, *args, **xargs):
-        self.eps = perturb_size
+class Sampler(ABC):
+    def __init__(self, *args, **kwargs):
+        return
 
-    def __call__(self, X, y, a, W, *args, **xargs):
+    @abstractmethod
+    def __call__(self):
+        """Samples a new estimate of boundary weights W
+
+        Typicially starts by sampling a perturbation X_ of current image X and
+        then uses direction (X_ - X) as update for W.
+        Returns the update W_ and preturbed sample X_.
+        If the sampler does not need to compute a perturbed X_ to update W_
+        (f.ex. OptimalSampler), then the returned X_ is a 0. array.
+
+        """
+        raise NotImplementedError
+
+    def __repr__(self):
+        string = 'Weight perturbation sampler with parameters:'
+        for key, val in self.__dict__.items():
+            string += '\n' + key + ': ' + str(val)
+        return string
+
+
+class PixelSampler(Sampler):
+    def __init__(self, perturb_size=.5, *args, **xargs):
+        super(PixelSampler, self).__init__()
+        self.perturb_size = perturb_size
+
+    def __call__(self, a, X, y, *args, **xargs):
         min_, max_ = a.bounds()
+        eps = self.perturb_size
 
         sgn = -1 if y == a.original_class else 1
         ix = self.sample_random_ix(X)
         dX = np.zeros_like(X)
         dX[ix] = 1.
 
-        X_ = np.clip(X + self.eps * dX, min_, max_)
+        X_ = np.clip(X + eps * dX, min_, max_)
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            dX = (X_ - X) / self.eps  # accounting for the clamping
-            return sgn * dX / self.eps
+            dX = (X_ - X) / eps  # accounting for the clamping
+            return sgn * dX / eps, X_
 
-        X_ = np.clip(X - self.eps * dX, min_, max_)
+        X_ = np.clip(X - eps * dX, min_, max_)
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            dX = - (X_ - X) / self.eps
-            return -sgn * dX / self.eps
+            dX = - (X_ - X) / eps
+            return -sgn * dX / eps, X_
 
-        return np.zeros_like(X)
+        return np.zeros_like(X), X_
 
     def sample_random_ix(self, X):
         ixs = []
@@ -43,12 +78,14 @@ class PixelSampler(object):
         return tuple(ixs)
 
 
-class GaussianSampler(object):
+class GaussianSampler(Sampler):
     def __init__(self, perturb_size=.5, *args, **xargs):
-        self.eps = perturb_size
+        super(GaussianSampler, self).__init__()
+        self.perturb_size = perturb_size
 
-    def __call__(self, X, y, a, W, *args, **xargs):
+    def __call__(self, a, X, y, *args, **xargs):
         min_, max_ = a.bounds()
+        eps = self.perturb_size
         X_o = a.original_image
 
         dX = np.random.randn(*X.shape).astype('float32')
@@ -59,28 +96,29 @@ class GaussianSampler(object):
         if np.sign((dX * (X_o - X)).sum()) < 0.:
             dX = -dX
 
-        X_ = np.clip(X + sgn * self.eps * dX, min_, max_)
+        X_ = np.clip(X + sgn * eps * dX, min_, max_)
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            dX = (X_ - X) / (sgn*self.eps)
-            return dX / self.eps
+            dX = (X_ - X) / (sgn*eps)
+            return dX / eps, X_
 
-        X_ = np.clip(X - sgn * self.eps * dX, min_, max_)
+        X_ = np.clip(X - sgn * eps * dX, min_, max_)
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            dX = - (X_ - X) / (sgn*self.eps)
-            return - dX / self.eps
+            dX = - (X_ - X) / (sgn*eps)
+            return - dX / eps, X_
 
-        return np.zeros_like(X)
+        return np.zeros_like(X), X_
 
 
-class OptimalSampler(object):
+class OptimalSampler(Sampler):
     def __init__(self, *args, **xargs):
+        super(OptimalSampler, self).__init__()
         return
 
-    def __call__(self, X, y, a, *args, **xargs):
+    def __call__(self, a, X, *args, **xargs):
         y_o = a.original_class
         y_a = a.adversarial_class
 
@@ -89,14 +127,14 @@ class OptimalSampler(object):
         in_gradient[y_a] = -1
 
         W_ = a.backward(in_gradient, X)  # grad(net(X)[y_o] - net(X)[y])
-        return W_
+        return W_, np.zeros_like(a.original_image)
 
 
 # ######################################################################## #
 # ######## WARNING: the following classes/functions use pytorch ########## #
 # ######################################################################## #
 
-class ImageSampler(object):
+class ImageSampler(Sampler):
     def __init__(self,
                  perturb_size=.5,
                  crop_size=None,
@@ -108,7 +146,8 @@ class ImageSampler(object):
         import torchvision
         import torchvision.transforms as transforms
 
-        self.eps = perturb_size
+        super(ImageSampler, self).__init__()
+        self.perturb_size = perturb_size
         self.crop_size = crop_size
 
         if loader is None:
@@ -128,8 +167,9 @@ class ImageSampler(object):
 
         self.dataiter = iter(self.loader)
 
-    def __call__(self, X, y, a, W, *args, **xargs):
+    def __call__(self, a, X, y, *args, **xargs):
         min_, max_ = a.bounds()
+        eps = self.perturb_size
         X_o = a.original_image
 
         try:
@@ -143,7 +183,6 @@ class ImageSampler(object):
         sgn = -1 if y == a.original_class else 1
 
         if self.crop_size is not None:
-            # TODO: check here
             # note that ||dX||_2 \neq 1 anymore if cropping
             dX = extract_img_patch(dX, size=self.crop_size)
 
@@ -151,24 +190,24 @@ class ImageSampler(object):
         if np.sign((dX * (X_o - X)).sum()) < 0.:
             dX = -dX
 
-        X_ = np.clip(X + sgn * self.eps * dX, min_, max_)
+        X_ = np.clip(X + sgn * eps * dX, min_, max_)
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            dX = (X_ - X) / (sgn*self.eps)
-            return dX / self.eps  # boundary normal direction estimate
+            dX = (X_ - X) / (sgn*eps)
+            return dX / eps, X_
 
-        X_ = np.clip(X - sgn * self.eps * dX, min_, max_)
+        X_ = np.clip(X - sgn * eps * dX, min_, max_)
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            dX = - (X_ - X) / (sgn*self.eps)
-            return - dX / self.eps  # boundary normal direction estimate
+            dX = - (X_ - X) / (sgn*eps)
+            return - dX / eps, X_
 
-        return np.zeros_like(X)
+        return np.zeros_like(X), X_
 
 
-class PlugPatchSampler(object):
+class PlugPatchSampler(Sampler):
     def __init__(self,
                  crop_size=5,
                  loader=None,
@@ -178,8 +217,8 @@ class PlugPatchSampler(object):
         import torch
         import torchvision
         import torchvision.transforms as transforms
-        # TODO: beware, check the image format (HWC or CHW)
 
+        super(PlugPatchSampler, self).__init__()
         self.crop_size = crop_size
 
         if loader is None:
@@ -199,7 +238,7 @@ class PlugPatchSampler(object):
 
         self.dataiter = iter(self.loader)
 
-    def __call__(self, X, y, a, W, *args, **xargs):
+    def __call__(self, a, X, y, *args, **xargs):
         min_, max_ = a.bounds()
 
         try:
@@ -208,7 +247,7 @@ class PlugPatchSampler(object):
             self.dataiter = iter(self.loader)
             dX, _ = next(self.dataiter)
 
-        dX = dX.detach.cpu().numpy()
+        dX = dX.squeeze(0).detach().cpu().numpy()
         # Note that ||dX||_2 \neq 1 anymore if cropping
         dX, mask = extract_img_patch(dX, size=self.crop_size, with_mask=True)
         X_ = X * (np.ones_like(X) - mask) + dX
@@ -219,25 +258,22 @@ class PlugPatchSampler(object):
         logits, _ = a.predictions(X_)
         y_ = get_label(logits)
         if y_ != y:
-            return sgn * dX  # boundary normal direction estimate
+            return sgn * dX, X_
 
-        return np.zeros_like(X)
+        return np.zeros_like(X), X_
 
 
-# TODO: check here
-# TODO: check for image format (HWC or CHW)
 def extract_img_patch(X, size=5, with_mask=False):
-    import torch
-
-    height = X.size(3)
-    ix1 = random.randint(0, height-size)
-    ixs1 = torch.arange(ix1, ix1+size)
-    mask1 = torch.zeros_like(X)
-    mask1[:, :, ixs1, :] = 1
-    ix2 = random.randint(0, height-size)
-    ixs2 = torch.arange(ix2, ix2+size)
-    mask2 = torch.zeros_like(X)
-    mask2[:, :, :, ixs2] = 1
+    # hw = height_width
+    hw = X.shape[1]  # holds with both HWC and CWH format
+    ix1 = random.randint(0, hw-size)
+    ixs1 = np.arange(ix1, ix1+size)
+    mask1 = np.zeros_like(X)
+    mask1[:, ixs1, :] = 1
+    ix2 = random.randint(0, hw-size)
+    ixs2 = np.arange(ix2, ix2+size)
+    mask2 = np.zeros_like(X)
+    mask2[:, :, ixs2] = 1
     mask = mask1 * mask2
     if with_mask:
         return X * mask, mask
